688b0dbe9c169e53f2bc82285691eb322c5ef080
LiuSihao <liusihao123@qq.com>
2013-12-03 00:11:59 +0800

    pdf render init

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '9d75b8522099bb3accb03801fa3d859bbca3662f5d216371917aa064d06ee2f4ac8664a7a6f1b73fc2e099ba989080c68a02303ee5aa7b28ca6548cfc9ba8e65'
---
+++ test/integration/pdf_delivery_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+class PdfDeliveryTest < ActionDispatch::IntegrationTest
+  test "pdf request sends a pdf as file" do
+    get another_path(format: :pdf)
+
+    assert_match "PDF", response.body
+    assert_equal "binary", headers["Content-Transfer-Encoding"]
+
+    assert_equal "attachment; filename=\"contents.pdf\"",
+      headers["Content-Disposition"]
+    assert_equal "application/pdf", headers["Content-Type"]
+
+  end
+end
